37d7d6282dc2c930da38a02ba30a6511e684ec62
BG-Sachith <bs.guruge@gmail.com>
2025-01-20 07:52:32 +0530

    fixes

--- index.js
+++ index.js
@@ -7,16 +7,15 @@ const PORT = 4000;
 require("dotenv").config({ path: ".env" });
 const app = express();
 var corsOptions = {
-  origin: ['https://html-to-pdf-fe-3i37.vercel.app', '64.29.17.65:443'],//["http://localhost:3000", "https://html-to-pdf-fe-3i37.vercel.app"],
-  optionsSuccessStatus: 200, // For legacy browser support,
+  origin: ['http://localhost:3000', 'https://html-to-pdf-fe-3i37.vercel.app'],
+  optionsSuccessStatus: 200,
   credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
 };
 
+app.use(cors(corsOptions));
 app.use(logger("dev"));
 app.use(bodyParser.json({ type: 'application/*+json' }))
 
-app.use(cors(corsOptions));
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ extended: false }));
 
