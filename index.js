b6c1a554a06e2781b937de383f03f18537ffcba6
BG-Sachith <bs.guruge@gmail.com>
2025-01-22 22:27:33 +0530

    fixes

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ require("dotenv").config({ path: ".env" });
 
 // const allowedOrigin = 'https://html-to-pdf-fe-3i37.vercel.app';//process.env.ALLOWED_ORIGIN || 'http://localhost:3000';
 const allowedOrigin = 'http://localhost:3000';//process.env.ALLOWED_ORIGIN || 'http://localhost:3000';
+const allowedOrigin = 'http://localhost:3000';//process.env.ALLOWED_ORIGIN || 'http://localhost:3000';
 
 const app = express();
 // var corsOptions = {
