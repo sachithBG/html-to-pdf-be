ed238ac75843b3f61384f15a257ca323ea8c3787
BG-Sachith <bs.guruge@gmail.com>
2025-01-23 08:26:39 +0530

    revert api

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ require("dotenv").config({ path: ".env" });
 
 // const allowedOrigin = 'https://html-to-pdf-fe-3i37.vercel.app';//process.env.ALLOWED_ORIGIN || 'http://localhost:3000';
 // const allowedOrigin = 'http://localhost:3000';//process.env.ALLOWED_ORIGIN || 'http://localhost:3000';
-const allowedOrigin = 'http://188.166.245.108';//process.env.ALLOWED_ORIGIN || 'http://localhost:3000';
+const allowedOrigin = 'http://157.230.56.160';//process.env.ALLOWED_ORIGIN || 'http://localhost:3000';
 
 const app = express();
 // var corsOptions = {
@@ -42,24 +42,24 @@ app.get("/", (req, res) => {
 });
 
 // Apply middleware for versioning before handling routes
-app.use('/api_/:version/pdf', require("./app/middleware/apiVersionMiddleware"));
+app.use('/api/:version/pdf', require("./app/middleware/apiVersionMiddleware"));
 
-app.use("/api_/v1/users", require("./app/routes/user.route"));
-app.use("/api_/v1/profiles", require("./app/routes/profile.route"));
-app.use("/api_/v1/organizations", require("./app/routes/organization.route"));
-app.use("/api_/v1/addons", require("./app/routes/addonManage.route"));
-app.use("/api_/v1/external-keys", require("./app/routes/externalKey.route"));
+app.use("/api/v1/users", require("./app/routes/user.route"));
+app.use("/api/v1/profiles", require("./app/routes/profile.route"));
+app.use("/api/v1/organizations", require("./app/routes/organization.route"));
+app.use("/api/v1/addons", require("./app/routes/addonManage.route"));
+app.use("/api/v1/external-keys", require("./app/routes/externalKey.route"));
 
-app.use("/api_/v1/tags", require("./app/routes/tagManage.route"));
-app.use("/api_/v1/dynamic-html-table", require("./app/routes/dynamicHtmlTable.route"));
+app.use("/api/v1/tags", require("./app/routes/tagManage.route"));
+app.use("/api/v1/dynamic-html-table", require("./app/routes/dynamicHtmlTable.route"));
 
-app.use("/api_/v1/pdf-templates", require("./app/routes/pdfManage.route"));
-app.use("/api_/v2/pdf", require("./app/routes/pdfGenerate.route"));
-app.use("/api_/v1/logs", require("./app/routes/requestManager.route"));
+app.use("/api/v1/pdf-templates", require("./app/routes/pdfManage.route"));
+app.use("/api/v2/pdf", require("./app/routes/pdfGenerate.route"));
+app.use("/api/v1/logs", require("./app/routes/requestManager.route"));
 
-app.use('/api_/v1/tokens', require("./app/routes/token.routes"));
-app.use('/api_/v1/media', require("./app/routes/mediaLocale.route"));
-app.use("/api_/v1/s3", require("./app/routes/s3.route"));
+app.use('/api/v1/tokens', require("./app/routes/token.routes"));
+app.use('/api/v1/media', require("./app/routes/mediaLocale.route"));
+app.use("/api/v1/s3", require("./app/routes/s3.route"));
 
 
 
